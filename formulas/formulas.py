844e36dc369190e93a939944c603cd7252a29c01
Ebag333 <ebagola@gmail.com>
2016-11-09 12:47:51 -0800

    documentation \o/

--- formulas/formulas.py
+++ formulas/formulas.py
@@ -7,12 +7,31 @@ class Formulas(object):
 
     @staticmethod
     def capacitor_shield_tick(maximum_amount, current_amount, recharge_rate, end_time=1000, start_time=0):
+        """
+        :param maximum_amount:  The size of the capacitor/shield in gigajoules (GJ)
+        :param current_amount:  The current level of the capacitor/shield in gigajoules (GJ)
+        :param recharge_rate:   Recharge time listed for the capacitor/shield, in milliseconds
+        :param end_time:        The length of time that we are running, in milliseconds
+        :param start_time:      The length of time that we are starting, in milliseconds (unlikely to be used)
+        :return:                Returns the new capacitor amount in giagjoules (GJ)
+
+        This function assumes nothing else is at play to change the values while it's being calculated.
+        Formula validated and confirmed by CCP Larrikin. <3
+        """
         tau = recharge_rate / 5.0
         time_diff = start_time - end_time
         return ((1.0 + (sqrt(current_amount / maximum_amount) - 1.0) * exp(time_diff / tau)) ** 2) * maximum_amount
 
     @staticmethod
     def capacitor_shield_regen_matrix(capacitor_amount, capacitor_time):
+        """
+        :param capacitor_amount:  The size of the capacitor/shield in gigajoules (GJ)
+        :param capacitor_time:    Recharge time listed for the capacitor/shield, in milliseconds
+        :return:                  A matrix with the percent, capacitor amount after the tick, and the delta
+
+        This function assumes nothing else is at play to change the values while it's being calculated.
+        Most useful to determine the percentage point that gives the most cap, or for graphing it.
+        """
         regen_matrix = []
         percent = 0
         while percent < 1:
