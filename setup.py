3441a0589c18d4b7756587b67dd0b7e05923d631
Ebag333 <ebagola@gmail.com>
2016-11-13 14:20:37 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+from pip.req import parse_requirements
+from setuptools import setup, find_packages
+from eos import __version__
+
+parsed_reqs = parse_requirements('requirements.txt', session=False)
+install_requires = [str(ir.req) for ir in parsed_reqs]
+
+setup(name='Gnosis',
+      version='1.0',
+      description='EVE Online Formulas and Simulations',
+      author='Ebag Trescientas',
+      author_email='ebagola@gmail.com',
+      url='https://github.com/Ebag333/Gnosis',
+      packages=find_packages(exclude='tests'),
+     )
