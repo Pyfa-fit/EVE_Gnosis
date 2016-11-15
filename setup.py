b162f4e3dd009a6093ad6d5e6d4deb410945dd97
Ebag333 <ebagola@gmail.com>
2016-11-15 10:43:56 -0800

    update tox.ini

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ parsed_reqs = parse_requirements('requirements.txt', session=False)
 install_requires = [str(ir.req) for ir in parsed_reqs]
 
 setup(name='EVE_Gnosis',
-      version='2016.11.15',
+      version='2016.11.15', # Year.Month.Day
       description='EVE Online Formulas and Simulations',
       author='Ebag Trescientas',
       author_email='ebagola@gmail.com',
