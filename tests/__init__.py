c5f887260ac592bc39a292594749164e53d8ac4e
Ebag333 <ebagola@gmail.com>
2016-11-21 23:01:50 -0800

    Fix path

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,4 +3,4 @@ import sys
 
 #  Add Gnosis module to python paths
 script_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.realpath(os.path.join(script_dir, '..', '..')))
+sys.path.append(os.path.realpath(os.path.join(script_dir, '..')))
